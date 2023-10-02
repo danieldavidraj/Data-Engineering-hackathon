241b539d0550691ee563fa6cad2c222ef4a405bd
Daniel Davidraj <danieldavidraj23@gmail.com>
2023-10-02 18:45:34 +0530

    feat: :sparkles: checkpoint 3: granularity

--- dags/databricks_dag.py
+++ dags/databricks_dag.py
@@ -1,41 +1,51 @@
 from airflow import DAG
 from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
-from airflow.utils.dates import days_ago
+from datetime import datetime, timedelta
 
-job_config = {
-    "source": "<data source connection related fields>",
+notebook_params = {
+    "source": '{ \
+        "start_date": "25/07/2017", \
+        "end_date": "30/07/2017" \
+    }',
     "granularity": "daily",
-    "kpis": [
-        {
-            "name": "Users count",
-            "sql": "approx_count_distinct(fullVisitorId, 0.03)"
-        },
-        {
-            "name": "Bounce count",
-            "sql": "sum(totals.bounces)"
-        },
-        {
-            "name": "Conversion rate",
-            "sql": "(sum(totals.transactions)/count(*))*100"
-        }
-    ],
-    "dimensions": [
-        "geoNetwork.country",
-        "device.browser",
-        "device.deviceCategory",
-        "device.operatingSystem"
-    ],
-    "depth": 2
+    "kpis": '[ \
+        { \
+            "name": "Users count", \
+            "sql": "approx_count_distinct(fullVisitorId, 0.03)" \
+        }, \
+        { \
+            "name": "Bounce count", \
+            "sql": "sum(totals.bounces)" \
+        }, \
+        { \
+            "name": "Conversion rate", \
+            "sql": "(sum(totals.transactions)/count(*))*100" \
+        } \
+    ]',
+    "dimensions": '[ \
+        "geoNetwork.country", \
+        "device.browser", \
+        "device.deviceCategory", \
+        "device.operatingSystem" \
+    ]',
+    "depth": "2"
 }
 
-with DAG('databricks_dag',
-  start_date = days_ago(2),
-  schedule_interval = None,
-  default_args = job_config
-  ) as dag:
+with DAG(
+    'databricks_dag',
+    start_date=datetime(2021, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+    default_args={
+        "email_on_failure": False,
+        "email_on_retry": False,
+        "retry_delay": timedelta(minutes=2),
+    },
+) as dag:
 
-  opr_run_now = DatabricksRunNowOperator(
-    task_id = 'run_now',
-    databricks_conn_id = 'databricks_default',
-    job_id = '466727288980718'
-  )
+    opr_run_now = DatabricksRunNowOperator(
+        task_id = 'run_now',
+        databricks_conn_id = 'databricks_default',
+        job_id = '510006363154351',
+        notebook_params = notebook_params,
+    )
